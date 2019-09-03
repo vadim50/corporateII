7fa32109f3eaed88f12c1ff87c0afbee01c78f5f
vadim <vadim50>
2019-09-03 15:06:41 +0300

    Lesson 75(Урок 21.Отображение на экран добавленного комментария.Часть 1)

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -79,7 +79,21 @@ class CommentController extends Controller
 
        $post->comments()->save($comment);
 
-        echo json_encode(['hello'=>'world']);
+
+       $comment->load('user');
+
+       $data['id'] = $comment->id;
+
+       $data['email'] = (!empty($data['email'])) ? $data['email'] : $comment->user->email;
+       $data['name'] = (!empty($data['name'])) ? $data['name'] : $comment->user->name;
+
+       $data['hash'] = md5($data['email']);
+
+       $view_comment = view(env('THEME').'.content_one_comment')->with('data',$data)->render();
+
+        return \Response::json(['success'=>true,'comment'=>$view_comment,'data'=>$data]);
+
+        exit();
     }
 
     /**

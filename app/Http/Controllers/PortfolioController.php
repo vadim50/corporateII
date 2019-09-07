903fdb127534eaa12dcdfbf97b3208095322ea1f
vadim <vadim50>
2019-09-07 15:47:23 +0300

    Lesson 77(Урок 23.Страница портфолио)

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,8 +3,49 @@
 namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Corp\Repositories\PortfoliosRepository;
 
-class PortfolioController extends Controller
+class PortfolioController extends SiteController
 {
     //
+
+	 public function __construct(
+        PortfoliosRepository $p_rep
+                                ){
+
+        parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
+
+        //$this->s_rep = $s_rep;
+        $this->p_rep = $p_rep;
+
+        $this->template = env('THEME').'.portfolios';
+    }
+
+     public function index()
+    {
+        //
+         $this->title = 'Портфолио';
+        $this->keywords = 'Портфолио';
+        $this->meta_desc = 'Портфолио';
+
+        $portfolios = $this->getPortfolios();
+       // dd($portfolios);
+      
+        $content = view(env('THEME').'.portfolios_content')
+        ->with('portfolios',$portfolios)->render();
+        $this->vars = array_add($this->vars,'content',$content);
+
+        return $this->renderOutput();
+    }
+
+    public function getPortfolios(){
+    	$portfolios = $this->p_rep->get('*',false,true);
+
+    	//dd($portfolios);
+    	if($portfolios){
+    		$portfolios->load('filter');
+    	}
+
+    	return $portfolios;
+    }
 }
